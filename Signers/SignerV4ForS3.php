8a9b7e57bf23c907d6f510d114a3358f8ed6c16f
Jérémy Derussé <jeremy@derusse.com>
2020-02-20 17:21:17 +0100

    Move S3 signer to S3 package (#160)

---
+++ Signers/SignerV4ForS3.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AsyncAws\S3\Signers;
+
+use AsyncAws\Core\Credentials\Credentials;
+use AsyncAws\Core\Signers\Request;
+use AsyncAws\Core\Signers\SignerV4;
+
+/**
+ * Version4 of signer dedicated for service S3.
+ *
+ * @author Jérémy Derussé <jeremy@derusse.com>
+ */
+class SignerV4ForS3 extends SignerV4
+{
+    public function sign(Request $request, ?Credentials $credentials): void
+    {
+        if (!$request->hasHeader('x-amz-content-sha256')) {
+            $request->setHeader('x-amz-content-sha256', $this->getCanonicalizedBody($request));
+        }
+        if (!$request->hasHeader('content-md5')) {
+            $request->setHeader('content-md5', base64_encode(hash('md5', $request->getBody(), true)));
+        }
+
+        parent::sign($request, $credentials);
+    }
+}
